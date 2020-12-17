5c9a45159cac120f242004342b97927ce4f054eb
Breno <brenodias002@gmail.com>
2020-12-17 16:18:21 -0300

    adcionar

--- oloco/app/controllers/produtos_controller.rb
+++ oloco/app/controllers/produtos_controller.rb
@@ -1,2 +1,13 @@
 class ProdutosController < ApplicationController
+
+  def index
+    @produtos = Produto.order(nome: :desc)
+    @produtos_com_desconto = Produto.order(:preco).limit 1
+  end
+
+  def crete
+    produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+    Produto.create produto
+  end
+
 end
