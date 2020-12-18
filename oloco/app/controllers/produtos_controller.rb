a2d894173e489d381a207e13244c9171301fdcb1
Breno <brenodias002@gmail.com>
2020-12-18 14:24:29 -0300

    arquivos

--- oloco/app/controllers/produtos_controller.rb
+++ oloco/app/controllers/produtos_controller.rb
@@ -1,13 +1,15 @@
 class ProdutosController < ApplicationController
 
   def index
-    @produtos = Produto.order(nome: :desc)
+    @produtos = Produto.order(nome: :desc).limit 10
     @produtos_com_desconto = Produto.order(:preco).limit 1
   end
 
-  def crete
+  def create
     produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
     Produto.create produto
+
+    redirect_to '/'
   end
 
 end
